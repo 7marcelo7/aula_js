3251949f88c3c1af51b00d9290426d1573e95059
7marcelo7 <martinsmathiasmarcelo@gmail.com>
2021-07-29 17:41:18 -0300

    added style.css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function clicked() {
+  document.getElementById('agradecimento').innerHTML =
+    '<b>Origado pela visista</b>'
+}
+function redirecionar() {
+  //window.open('http://127.0.0.1:5501/index.html')
+  window.location.href = 'http://127.0.0.1:5501/index.html'
+}
